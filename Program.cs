bf2e85ed1a0891950b3d848fecaf99ff77d75678
afarel454 <afareladityaputra561@gmail.com>
2025-06-16 20:39:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿public class Karyawan
+{
+    private string nama;
+    private string id;
+    private double gajiPokok;
+
+    public Karyawan(string nama, string id, double gajiPokok)
+    {
+        this.nama = nama;
+        this.id = id;
+        this.gajiPokok = gajiPokok;
+    }
+
+    public string GetNama() => nama;
+    public void SetNama(string nama) => this.nama = nama;
+
+    public string GetId() => id;
+    public void SetId(string id) => this.id = id;
+
+    public double GetGajiPokok() => gajiPokok;
+    public void SetGajiPokok(double gajiPokok) => this.gajiPokok = gajiPokok;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Nama: " + nama);
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Gaji Pokok: " + gajiPokok);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Karyawan k = new Karyawan("Advent", "001", 5000000);
+        k.DisplayInfo();
+    }
+}
